c36ed563a6b358573e0db2781ffdcc3cbcf5d623
Jordi Exposito <exjoru220806@gmail.com>
2024-04-16 13:31:00 +0200

    Finalizado jQuery

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+ $('#boton1').on('click', function() {
+     $('#text1').toggleClass('red1 blue1');
+ });
+ 
+ $('#boton2').on('click', function() {
+     $('#text2').toggleClass('red2 blue2');
+ });
+});
